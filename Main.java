dcd0f0ef671cf03ce512f04519d9288ba76674a8
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-06 16:25:29 +0000

    Added an example of the .concat() method

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main (String[] args){
         String newStr = "Hello, World!";//intialise a string
+        String oldStr = "Goodbye, World!";
         System.out.println(newStr);//print the string
         /*Strings are objects.
          * This means that you can use methods to perform operations on them, for example;
@@ -11,6 +12,7 @@ public class Main {
         //the next one is slightly less obvious, but still...
         System.out.println(newStr.indexOf("World!"));//should return 7 if I'm right which is probable (I think?). 
         //I was right.
+        System.out.println(newStr.concat(oldStr));//concatenates the two strings using a method.
         //so that's kind of cool, especially given I'd never thought of strings as objects before...
         //there will be more once i do more java
     }
